c233eca9f31a8717f5fb429817891b8636ae3875
Natalia Goyes <n.gg.1309@gmail.com>
2022-10-24 14:00:20 -0500

    first commit

---
+++ Clase1MasterDev/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Clase1MasterDev
+//
+//  Created by Natalia Goyes on 24/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    /*fff*/
+
+
+
+}
+
